c86361c12371188c2fd4437ec5b81a23720e33ca
Subrata-Shuvo <himis946@gmail.com>
2021-08-17 14:02:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// getElementById
+
+const myHeading1 = document.getElementById('heading1')
+myHeading1.innerHTML = "Good Job";
+
+document.getElementById('heading2').innerHTML = "Let's learn dom"
+
+// getElementByTagName
+
+// document.getElementsByTagName("p")[1].innerHTML = "I am a paragraph"
+
+// getElementsByClassName
